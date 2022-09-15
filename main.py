fe42386ffb00c08ead63c61b642e93180eab11e3
vilius.kulinas@gmail.com <vilius.kulinas@LDN-7839.local>
2022-09-15 09:38:14 +0100

    1st task primative way...

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import json
 
 with open("assets/user.json") as f:
     data = json.load(f)
-    print(data)
+
+for user in data:
+    if "full_name" in user:
+        continue
+    else:
+        user["full_name"] = f"{user['forename']} {user['surname']}"
+
+print(data)
