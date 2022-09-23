2a9c489aac958768b78defbfb11cc716977d5926
vilius.kulinas@gmail.com <vilius.kulinas@LDN-7839.local>
2022-09-23 09:55:32 +0100

    _age() made @staticmethod, added some tests for _age(), not sure about testing if ValueError is rised...commit for the sake of meeting

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,8 +3,8 @@ from users import Users
 
 
 def test_add_full_name_with_correct_data(users_correct_data):
-
     result = users_correct_data.add_full_name()
+
     assert isinstance(result, list)
     assert len(result) == 2
     assert "full_name" in result[0].keys()
@@ -12,15 +12,21 @@ def test_add_full_name_with_correct_data(users_correct_data):
 
 
 def test_add_full_name_with_faulty_data(users_faulty_data):
-
     result = users_faulty_data.add_full_name()
+
     assert result[0]["full_name"] == "Jane "
     assert result[1]["full_name"] == " Johnson"
     assert result[2]["full_name"] == "Jane Doe"
     assert result[3]["full_name"] == " "
 
 
-def test_age_with_correct_data(users_correct_data):
-
-    result = users_correct_data._age({"date_of_birth": "2001/10/12"})
+def test_age_with_correct_data():
+    correct_dob = {"date_of_birth": "2001/10/12"}
+    result = Users._age(correct_dob)
     assert result == 20
+
+
+def test_age_with_faulty_data():
+    incorrect_data = {"date_of_birth": ""}
+    with pytest.raises(ValueError):
+        Users._age(incorrect_data)
