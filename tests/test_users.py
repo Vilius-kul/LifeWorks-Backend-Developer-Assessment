8e04870cf451c2ef2337846868b87f54b4e89b3c
vilius.kulinas@gmail.com <vilius.kulinas@LDN-7839.local>
2022-09-23 10:50:05 +0100

    for meeting sake, added test_thirty_and_over

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pytest
 from users import Users
 
@@ -28,5 +30,12 @@ def test_age_with_correct_data():
 
 def test_age_with_faulty_data():
     incorrect_data = {"date_of_birth": ""}
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as ve:
         Users._age(incorrect_data)
+    assert "time data '' does not match format '%Y/%m/%d'" in str(ve.value)
+
+
+def test_thirty_and_over_with_correct_data(users_correct_data):
+    result = users_correct_data.thirty_and_over()
+    for user in result:
+        assert Users._age(user) >= 30
